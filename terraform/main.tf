e6ce211ab006939b77a326cde51d3f556e123c95
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-06-16 15:42:57 -0400

    provider(version-constraint): remove for prov blk

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,6 @@ terraform {
 }
 
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = ">=4.0.2"
   features {}
 }
 
